4b4bcf4f40b744ece4d6b94c6b95855f17d5ee20
BLeno <greenwood.joechan@hotmail.com>
2022-10-20 22:03:01 +0100

    Project 3

---
+++ Project_3/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+        bucket  = "comeonjames-2022"
+        key     = "network/terraform-demo.tfstate"
+        region = "eu-west-2"
+        dynamodb_table = "lockingmate"
+    }
+
+}
---
+++ Project_3/output.tf
@@ -0,0 +1,7 @@
+output "SUBNET_ID"{
+    value = module.myapp-subnet.subnet.id
+}
+
+output "Instance_IP" {
+    value = module.myapp-server.instance_ip
+}
